e4e9834d1a049b8f01c8b3c75498b5bd384c3b4a
darkeclipz <larsrotgers@gmail.com>
2019-09-30 21:52:27 +0200

    typos

--- Programming/Project Euler/PyCharm Project/integer_right_triangles.py
+++ Programming/Project Euler/PyCharm Project/integer_right_triangles.py
@@ -11,7 +11,7 @@ where v > u, v and u are not both odd, and gcd(v,u) = 1.
 Let a + b + c = P, then P = 2v^2 + 2uv. This gives us a formula
 to generate the perimeter P of a primitive Pythagorean Triplet.
 
-We still need to calculate all the multiples of that triples but
+We still need to calculate all the multiples of that triplet but
 because m(a + b + c) = mP, we can simply multiply P by a multiple
 of m, where m >= 1 and mP <= 1000.
 
@@ -26,12 +26,12 @@ def integer_right_triangles(n):
     u = 1
     p = 12  # [3, 4, 5] has perimeter 12
     while u**2 <= n:
-        if p <= 1000 and not is_odd(v) and is_odd(u):
+        if p <= n and not is_odd(v) and is_odd(u):
             # u, v combination is a primitive triplet
             a = 1
             q = p
             # find all multiples and increment solutions
-            while q <= 1000:
+            while q <= n:
                 solutions[q] += 1
                 a += 1
                 q = a * p
